97c9dd52594bdffd29024f9e83450fcd419b86ee
John Butler <johnnybutler7@gmail.com>
2021-08-05 19:03:38 +0100

    Adds exchange ether balance to UI

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,7 +1,8 @@
 class AccountsController < ApplicationController
   def index
     @account = BlOCKCHAIN_CLIENT.eth_accounts['result'][0]
-    @balance = Ethereum::Formatter.new.from_wei(BlOCKCHAIN_CLIENT.eth_get_balance(@account)['result'].hex)
+    @ether_balance = Ethereum::Formatter.new.from_wei(BlOCKCHAIN_CLIENT.eth_get_balance(@account)['result'].hex)
+    @exchange_ether_balance = Ethereum::Formatter.new.from_wei(EXCHANGE.call.balance_of(ENV['ETHER_ADDRESS'], @account))
     
     trade_events = Blockchain::Logs.new(contract: EXCHANGE, event_name: 'Trade').call
     @trades = Blockchain::Events.new(events: trade_events).trades
