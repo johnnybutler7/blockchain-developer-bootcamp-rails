6a72e0069a74776fe9c762f8e448d910efeab3f6
John Butler <johnnybutler7@gmail.com>
2021-08-06 18:35:41 +0100

    adds eth & token balances to UI

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,13 +3,14 @@ class AccountsController < ApplicationController
     @account = BlOCKCHAIN_CLIENT.eth_accounts['result'][0]
     @ether_balance = Ethereum::Formatter.new.from_wei(BlOCKCHAIN_CLIENT.eth_get_balance(@account)['result'].hex)
     @exchange_ether_balance = Ethereum::Formatter.new.from_wei(EXCHANGE.call.balance_of(ENV['ETHER_ADDRESS'], @account))
-    
+    @token_balance = TOKEN.call.balance_of(@account)
+    @exchange_token_balance = EXCHANGE.call.balance_of(TOKEN.address, @account)
+
     trade_events = Blockchain::Logs.new(contract: EXCHANGE, event_name: 'Trade').call
     @trades = Blockchain::Events.new(events: trade_events).trades
     
     order_events = Blockchain::Logs.new(contract: EXCHANGE, event_name: 'Order').call
     open_orders = Blockchain::Events.new(events: order_events).open_orders
-    
     @sell_orders = open_orders.find_all{|o| o[:orderType] == 'sell'}
     @buy_orders = open_orders.find_all{|o| o[:orderType] == 'buy'}
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def format_token_balance(val)
+    precision = 100
+    val = Ethereum::Formatter.new.from_wei(val).to_f.to_i
+    val = (val * precision).round / precision
+    val
+  end
 end
