f707d9ab64fb3f1169ef61cc0c5eae979228752a
John Butler <johnnybutler7@gmail.com>
2021-08-11 15:51:00 +0100

    refactor ether deposit functionality into a service

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -1,28 +1,6 @@
 class DepositsController < ApplicationController
-  def create    
-    etherAmount = Ethereum::Formatter.new.to_wei(params[:ether_amount].to_i)
-    account = BlOCKCHAIN_CLIENT.eth_accounts['result'][0]
-    
-    key = Eth::Key.new priv: ENV['ACCOUNT_PRIVATE_KEY']
-    function_name = 'depositEther'
-    function = EXCHANGE.parent.functions.find { |f| f.name == function_name }
-    abi = EXCHANGE.abi.find { |abi| abi['name'] == function_name }
-    encoder = Ethereum::Encoder.new
-    inputs = abi['inputs'].map { |input| OpenStruct.new(input) }
-    input = encoder.encode_arguments(inputs, [])
-    data = encoder.ensure_prefix(function.signature + input)
-    tx_args = {
-      from: account,
-      to: EXCHANGE.address,
-      data: data,
-      value: etherAmount,
-      nonce: BlOCKCHAIN_CLIENT.get_nonce(key.address),
-      gas_limit: BlOCKCHAIN_CLIENT.gas_limit,
-      gas_price: BlOCKCHAIN_CLIENT.gas_price
-    }
-    tx = Eth::Tx.new(tx_args)
-    tx.sign(key)
-    BlOCKCHAIN_CLIENT.eth_send_raw_transaction(tx.hex)
+  def create
+    Blockchain::EtherDepositor.new(amount: Ethereum::Formatter.new.to_wei(params[:ether_amount].to_i)).call
 
     redirect_to accounts_path, notice: 'Successfully deposited Ether'
   end
---
+++ app/services/blockchain/ether_depositor.rb
@@ -0,0 +1,57 @@
+module Blockchain
+  class EtherDepositor
+    def initialize(amount:)
+      @amount = amount
+      @function_name = 'depositEther'
+    end
+  
+    def call
+      transaction = build_transaction
+      transaction.sign(key)
+      BlOCKCHAIN_CLIENT.eth_send_raw_transaction(transaction.hex)
+    end
+  
+    private
+  
+    attr_reader :function_name, :amount
+  
+    def build_transaction
+      Eth::Tx.new(tx_args)
+    end
+  
+    def key
+      Eth::Key.new priv: ENV['ACCOUNT_PRIVATE_KEY']
+    end
+  
+    def tx_args
+      {
+        from: account,
+        to: EXCHANGE.address,
+        data: data,
+        value: amount,
+        nonce: BlOCKCHAIN_CLIENT.get_nonce(key.address),
+        gas_limit: BlOCKCHAIN_CLIENT.gas_limit,
+        gas_price: BlOCKCHAIN_CLIENT.gas_price
+      }
+    end
+  
+    def account
+      BlOCKCHAIN_CLIENT.eth_accounts['result'][0]
+    end
+  
+    def function
+      EXCHANGE.parent.functions.find { |f| f.name == function_name }
+    end
+
+    def data
+      encoder = Ethereum::Encoder.new
+      inputs = abi['inputs'].map { |input| OpenStruct.new(input) }
+      input = encoder.encode_arguments(inputs, [])
+      encoder.ensure_prefix(function.signature + input)
+    end
+  
+    def abi
+      EXCHANGE.abi.find { |abi| abi['name'] == function_name }
+    end
+  end
+end
