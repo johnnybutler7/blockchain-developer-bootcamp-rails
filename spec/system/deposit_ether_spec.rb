5da4aec67e094d16761c9cad7c9e32a50346cce2
John Butler <johnnybutler7@gmail.com>
2021-08-11 15:24:20 +0100

    adds spec for deposit ether

---
+++ spec/system/deposit_ether_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature 'Deposit Ether', :type => :system do
+
+  it "Enables me to deposit Ether" do
+    deposit_eth_amount = 2
+    
+    visit accounts_path
+    start_eth_amount = find('#exchange-ether-balance').text.to_f
+    fill_in 'ether_amount', with: deposit_eth_amount
+    click_on 'Deposit'
+    
+    expect(page).to have_content('Successfully deposited Ether')
+    within('#exchange-ether-balance') do
+      expect(page).to have_content(start_eth_amount + deposit_eth_amount)
+    end
+  end
+end
