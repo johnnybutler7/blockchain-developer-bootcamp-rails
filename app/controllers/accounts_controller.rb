5666ca047b37761de12a9d42da0ad06b6f670b70
John Butler <johnnybutler7@gmail.com>
2021-08-21 12:56:48 +0100

    Rework account header partial to show correct account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,10 +1,9 @@
 class AccountsController < ApplicationController
   def index
-    @account = BlOCKCHAIN_CLIENT.eth_accounts['result'][0]
-    @ether_balance = Ethereum::Formatter.new.from_wei(BlOCKCHAIN_CLIENT.eth_get_balance(@account)['result'].hex)
-    @exchange_ether_balance = Ethereum::Formatter.new.from_wei(EXCHANGE.call.balance_of(ENV['ETHER_ADDRESS'], @account))
-    @token_balance = TOKEN.call.balance_of(@account)
-    @exchange_token_balance = EXCHANGE.call.balance_of(TOKEN.address, @account)
+    @ether_balance = Ethereum::Formatter.new.from_wei(BlOCKCHAIN_CLIENT.eth_get_balance(@current_account)['result'].hex)
+    @exchange_ether_balance = Ethereum::Formatter.new.from_wei(EXCHANGE.call.balance_of(ENV['ETHER_ADDRESS'], @current_account))
+    @token_balance = TOKEN.call.balance_of(@current_account)
+    @exchange_token_balance = EXCHANGE.call.balance_of(TOKEN.address, @current_account)
 
     trade_events = Blockchain::Logs.new(contract: EXCHANGE, event_name: 'Trade').call
     @trades = Blockchain::Events.new(events: trade_events).trades
@@ -14,9 +13,7 @@ class AccountsController < ApplicationController
     @sell_orders = open_orders.find_all{|o| o[:orderType] == 'sell'}
     @buy_orders = open_orders.find_all{|o| o[:orderType] == 'buy'}
 
-    @my_orders = open_orders.find_all{|o| o[:user] == @account}
-    @my_trades = @trades.find_all{|o| o[:user] == @account || o[:userFill] == @account}
-    
-   # byebug
+    @my_orders = open_orders.find_all{|o| o[:user] == @current_account}
+    @my_trades = @trades.find_all{|o| o[:user] == @current_account || o[:userFill] == @current_account}
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :load_current_account
+  
+  private
+  
+  def load_current_account
+    @current_account = BlOCKCHAIN_CLIENT.eth_accounts['result'][0]
+  end
 end
