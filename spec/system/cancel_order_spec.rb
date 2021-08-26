9cd1cdf95c6ae139795263659a348c453ab4c5ef
John Butler <johnnybutler7@gmail.com>
2021-08-26 17:38:18 +0100

    Convert cancel link_to to button_to as per hotwire updated(no UJS)

--- spec/system/cancel_order_spec.rb
+++ spec/system/cancel_order_spec.rb
@@ -5,13 +5,15 @@ RSpec.feature 'Cancel Order', :type => :system do
   it "Enables me to cancel an order" do
     visit accounts_path
     click_on "Orders"
-    cancel_order_link  = first(:link, "X")
+    cancel_order_link  = first(:button, "X")
     cancel_order_id = cancel_order_link["data-order-id"] 
     
     cancel_order_link.click
-    click_on "Orders"
     
-    expect(page).to have_content('Order successfully cancelled')
+    within('.notices') do
+      expect(page).to have_content('Order successfully cancelled')
+    end
+    click_on "Orders"
     within('#my-orders') do
       expect(page).to_not have_selector(:css, "tr#cancel-order-#{cancel_order_id}")
     end
