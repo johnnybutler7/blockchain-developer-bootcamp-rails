519a4edd7914c236acba78ca70a43da489c34290
John Butler <johnnybutler7@gmail.com>
2021-08-26 16:51:31 +0100

    add buy order trade to general trades partial, fix spec

--- spec/system/fill_order_spec.rb
+++ spec/system/fill_order_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature 'Fill Order', :type => :system do
 
-  it "Enables me to fill an order" do
+  it "Enables me to fill a sell order" do
     visit accounts_path
     sell_order_id = ''
     
@@ -19,6 +19,28 @@ RSpec.feature 'Fill Order', :type => :system do
     end
     within('#order-book') do
       expect(page).to_not have_selector(:css, "tr#sell-order-#{sell_order_id}")
+    end
+  end
+  
+  it "Enables me to fill a buy order" do
+    visit accounts_path
+    buy_order_id = ''
+    
+    within('#order-book') do
+      buy_order_link = find(".buy-order", match: :first)
+      buy_order_id = buy_order_link["data-order-id"] 
+    
+      buy_order_link.click
+    end
+    
+    within('.notices') do
+      expect(page).to have_content('Order successfully filled')
+    end
+    within('#my-trades') do
+      expect(page).to have_selector(:css, "tr#my-trade-#{buy_order_id}")
+    end
+    within('#order-book') do
+      expect(page).to_not have_selector(:css, "tr#buy-order-#{buy_order_id}")
     end
   end
 end
