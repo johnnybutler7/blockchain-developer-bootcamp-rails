21ba5f2f5ec0727354526954b8c46334d27fce98
John Butler <johnnybutler7@gmail.com>
2021-08-29 14:37:04 +0100

    refactor open orders query spec

--- spec/queries/dapp/open_orders_query_spec.rb
+++ spec/queries/dapp/open_orders_query_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 RSpec.describe Dapp::OpenOrdersQuery, type: :query do
   context 'all open orders' do
     before(:each) do
-      allow(EXCHANGE.call).to receive(:order_filled).with(1).and_return(false)
-      allow(EXCHANGE.call).to receive(:order_cancelled).with(1).and_return(false)
+      allow_any_instance_of(Dapp::OpenOrderPolicy).to receive(:open?).and_return(true)
       @queried_orders = Dapp::OpenOrdersQuery.new(orders: orders).execute
     end
     
@@ -12,22 +11,9 @@ RSpec.describe Dapp::OpenOrdersQuery, type: :query do
     end
   end
   
-  context 'filled orders' do
+  context 'non open order' do
     before(:each) do
-      allow(EXCHANGE.call).to receive(:order_filled).with(1).and_return(true)
-      allow(EXCHANGE.call).to receive(:order_cancelled).with(1).and_return(false)
-      @queried_orders = Dapp::OpenOrdersQuery.new(orders: orders).execute
-    end
-    
-    it '.orders' do
-      expect(@queried_orders.size).to eql 0
-    end
-  end
-  
-  context 'cancelled orders' do
-    before(:each) do
-      allow(EXCHANGE.call).to receive(:order_filled).with(1).and_return(false)
-      allow(EXCHANGE.call).to receive(:order_cancelled).with(1).and_return(true)
+      allow_any_instance_of(Dapp::OpenOrderPolicy).to receive(:open?).and_return(false)
       @queried_orders = Dapp::OpenOrdersQuery.new(orders: orders).execute
     end
     
