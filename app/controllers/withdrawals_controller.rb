cc433e0cb392921cf79df600ce06a0aefa296c4d
John Butler <johnnybutler7@gmail.com>
2021-09-04 07:06:55 +0100

    Shor error messages via toast

--- app/controllers/withdrawals_controller.rb
+++ app/controllers/withdrawals_controller.rb
@@ -3,16 +3,20 @@ class WithdrawalsController < ApplicationController
   def create
     ether_withdraw = Dapp::EtherWithdraw.new(amount: Ethereum::Formatter.new.to_wei(ether_amount))
     result = Blockchain::Runner.new(transaction: ether_withdraw).run
+    @notice_at = Time.now
 
     respond_to do |format|
       if result.success?
         format.turbo_stream {
           @dapp_status = Dapp::Status.new
-          @notice_at = Time.now
         }
         format.html { redirect_to dapp_path, notice: 'Successfully withdrew Ether' }  
       else
         format.html { redirect_to dapp_path, notice: "There was a problem withdrawing Ether - #{result.error}" }
+        format.turbo_stream do
+            render turbo_stream: turbo_stream.append("toasts", partial: "shared/toast",
+               locals: { message: "There was a problem withdrawing Ether - #{result.error}", notice_at: @notice_at })
+          end
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resource :dapp, only: [:show]
+  resource :dapp, only: [:show], controller: 'dapp'
   resources :orders, only: [:create, :destroy, :update]
   resources :sell_orders, only: [:create]
   resources :deposits, only: [:create]
